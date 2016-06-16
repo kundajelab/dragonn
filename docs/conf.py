d186ada1ea82efb0cf6e52c2e6ac3cd971015b5e
jisraeli <jisraeli@stanford.edu>
2016-06-16 11:51:59 -0700

    removed mocked imports, added path to dragonn package, updated sphnix build directory

--- docs/conf.py
+++ docs/conf.py
@@ -15,18 +15,11 @@
 import sys
 import os
 import shlex
-# making sphnix compile with RTD using mock
-import mock
- 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
-                'deeplift', 'keras', 'theano']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('../dragonn'))
+sys.path.insert(0, os.path.abspath('../dragonn'))
 
 # -- General configuration ------------------------------------------------
 
