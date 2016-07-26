43f899bd21dff162c711b0f986a3784911fbb26e
jisraeli <jisraeli@stanford.edu>
2016-07-25 18:12:15 -0700

    added download/upload scripts for model zoo

---
+++ scripts/download_model_from_gist.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# Adapted from caffe source code
+# License: https://github.com/BVLC/caffe/blob/master/LICENSE
+
+GIST=$1
+DIRNAME=${2:-./models}
+
+if [ -z $GIST ]; then
+  echo "usage: download_model_from_gist.sh <gist_id> <dirname>"
+  exit
+fi
+
+GIST_DIR=$(echo $GIST | tr '/' '-')
+MODEL_DIR="$DIRNAME/$GIST_DIR"
+
+if [ -d $MODEL_DIR ]; then
+    echo "$MODEL_DIR already exists! Please make sure you're not overwriting anything important!"
+    exit
+fi
+
+echo "Downloading DragoNN model info to $MODEL_DIR ..."
+mkdir -p $MODEL_DIR
+wget https://gist.github.com/$GIST/download -O $MODEL_DIR/gist.zip
+unzip -j $MODEL_DIR/gist.zip -d $MODEL_DIR
+rm $MODEL_DIR/gist.zip
+echo "Done"
