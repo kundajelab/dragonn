9350d2c74bfc2339927232dcb6b2aeb3986e150d
jisraeli <jisraeli@stanford.edu>
2016-12-29 09:35:02 -0800

    updated simdna version to 0.1.2

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ config = {
     'version': '0.1.2',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy>=1.9', 'keras==0.3.3', 'deeplift==0.3', 'shapely', 'simdna==0.1', 'matplotlib<=1.5.3',
+    'install_requires': ['numpy>=1.9', 'keras==0.3.3', 'deeplift==0.3', 'shapely', 'simdna==0.2', 'matplotlib<=1.5.3',
                          'scikit-learn', 'pydot_ng==1.0.0', 'h5py'],
     'dependency_links': ["https://github.com/kundajelab/deeplift/tarball/v0.3-alpha#egg=deeplift-0.3",
-                         "https://github.com/kundajelab/simdna/tarball/0.1#egg=simdna-0.1"],
+                         "https://github.com/kundajelab/simdna/tarball/0.2#egg=simdna-0.2"],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__main__:main']},
     'name': 'dragonn'
