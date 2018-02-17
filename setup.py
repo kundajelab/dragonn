389f2886e235831782c902b2cdbca15e481faf7a
anna shcherbina <annashcherbina@gmail.com>
2018-02-16 21:56:40 -0800

    updated tensorflow dep in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'version': '0.1.3',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy>=1.9','tensorflow-gpu==1.0.0', 'keras==1.2.2', 'deeplift==0.4.0-tensorflow', 'shapely', 'simdna==0.3', 'matplotlib<=1.5.3',
+    'install_requires': ['numpy>=1.9','tensorflow-gpu==1.0.1', 'keras==1.2.2', 'deeplift==0.4.0-tensorflow', 'shapely', 'simdna==0.3', 'matplotlib<=1.5.3',
                          'scikit-learn', 'pydot_ng==1.0.0', 'h5py'],
     'dependency_links': ["https://github.com/kundajelab/deeplift/tarball/v0.4.0-tensorflow#egg=deeplift-0.4.0-tensorflow",
                          "https://github.com/kundajelab/simdna/tarball/0.3#egg=simdna-0.3"],
