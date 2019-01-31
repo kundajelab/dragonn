ee3da8cbf73c95c09ae4ed7fad8d93c2fc3bcd64
anna shcherbina <annashcherbina@gmail.com>
2019-01-31 10:40:21 -0800

    genomewide training helper files

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ config = {
     'packages': ['dragonn'],
     'setup_requires': [],
     'install_requires': ['numpy>=1.9', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'simdna==0.3', 'matplotlib',
-                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise'],
+                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader'],
     'extras_requires':{'tensorflow with gpu':['tensorflow-gpu>=1.7']},
     'dependency_links': ["https://github.com/kundajelab/simdna/tarball/0.3#egg=simdna-0.3",
-                         "https://github.com/kundajelab/deeplift.git"],
+                         "https://github.com/kundajelab/deeplift.git",
+                         "https://github.com/kundajelab/seqdataloader"],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__main__:main']},
     'name': 'dragonn'
