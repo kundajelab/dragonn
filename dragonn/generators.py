830e34d0ed1436652d985a4201bc8adac0000c5c
anna shcherbina <annashcherbina@gmail.com>
2019-02-06 10:56:59 -0800

    rewoked generators to 1) shuffle samples after each epoch 2) ensure thread safety by adding locking,s such that fit_generator can be run with multiprocessing=True and workers >1

--- dragonn/generators.py
+++ dragonn/generators.py
@@ -1,9 +1,11 @@
-from argparse import Namespace
-import numpy as np
+from keras.utils import Sequence
 import pandas as pd
-import pysam
+import numpy as np
 import random
+import math 
+import pysam
 from .utils import ltrdict
+import threading 
 
 def dinuc_shuffle(seq):
     #get list of dinucleotides
@@ -30,45 +32,74 @@ def revcomp(seq):
             rc.append(base)
     return ''.join(rc)
 
-    
-def shuffled_ref_generator(data_path,ref_fasta,batch_size=128,add_revcomp=True,tasks=None):
-    '''
-    Generates tuple([x_batch, y_batch]). x_batch is a numpy ndarray (batch_size, 1, seq_length,4). y_batch is an ndarray (batch_size,ntasks). regions from data_path are assumed to be positives. These are dinucleotide-shuffled to generate corresponding negatives. 
-
-    Inputs: 
-    data_path: string containing path to the bed file (with task names in header) of the positive regions to use for training 
-    ref_fasta: .fa.gz or .fa file containing the reference genome. 
-    batch_size: batch_size for input to keras model training. Default=128 
-    add_revcomp: Boolean indicating whether or not reverse-complement sequences should be extracted for entries in each batch. Default=True 
-    tasks: a list of task names to extract from the input bed file (i.e. useful if you only want to train on a subset of the tasks). Default=None means that all tasks will be extracted from data_path. 
-    '''
-    #open the reference file
-    ref=pysam.FastaFile(ref_fasta)
-
-    #read in the label bed file 
-    data=pd.read_csv(data_path,header=0,sep='\t',index_col=[0,1,2])
-    if tasks!=None:
-        data=data[tasks]
-    #iterate through batches and one-hot-encode on the fly
-    start_index=0
-    num_generated=0
-    total_entries=data.shape[0]-batch_size
-    
-    #decide if reverse complement should be used
-    if add_revcomp==True:
-        batch_size=batch_size/4
-    else:
-        batch_size=batch_size
+#use wrappers for keras Sequence generator class to allow batch shuffling upon epoch end
+class DataGenerator(Sequence):
+    def __init__(self,data_path,ref_fasta,batch_size=128,add_revcomp=True,tasks=None,shuffled_ref_negatives=False,upsample=True,upsample_ratio=0.1):
+        #make our generator thread-safe to use the multiprocessing flag in keras fit_generator
+        self.lock=threading.lock() 
         
-    while True:
-        if (num_generated >=total_entries):
-            start_index=0
-        end_index=start_index+int(batch_size)
-        #get seq positions
-        bed_entries=data.index[start_index:end_index]
-        #get sequences
-        seqs=[ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
+        self.batch_size=batch_size
+        #decide if reverse complement should be used
+        self.add_revcomp=add_revcomp
         if add_revcomp==True:
+            self.batch_size=int(batch_size/2)
+
+        #determine whether negative set should consist of the shuffled refs.
+        # If so, split batch size in 2, as each batch will be augmented with shuffled ref negatives
+        # in ratio equal to positives 
+        self.shuffled_ref_negatives=shuffled_ref_negatives
+        if self.shuffled_ref_negatives==True:
+            self.batch_size=int(self.batch_size/2)
+            
+        #open the reference file
+        self.ref=pysam.FastaFile(ref_fasta)
+        
+        #read in the label bed file 
+        data=pd.read_csv(data_path,header=0,sep='\t',index_col=[0,1,2])
+        if tasks!=None:
+            data=data[tasks]
+        self.data=data
+        
+        self.indices=np.arange(int(math.floor(self.data.shape[0]/self.batch_size)))
+        num_indices=self.indices.shape[0]
+        self.add_revcomp=add_revcomp
+        
+        #set variables needed for upsampling the positives
+        self.upsample=upsample
+        if self.upsample==True:
+            self.upsample_ratio=upsample_ratio
+            self.ones = self.data.loc[(self.data > 0).any(axis=1)]
+            self.zeros = self.data.loc[(self.data < 1).all(axis=1)]
+            self.pos_batch_size = int(self.batch_size * self.upsample_ratio)
+            self.neg_batch_size = self.batch_size - self.pos_batch_size
+            self.pos_indices=np.arange(int(math.floor(self.ones.shape[0]/self.pos_batch_size)))
+            self.neg_indices=np.arange(int(math.floor(self.zeros.shape[0]/self.neg_batch_size)))
+            
+            #wrap the positive and negative indices to reach size of self.indices
+            num_pos_wraps=math.ceil(num_indices/self.pos_indices.shape[0])
+            num_neg_wraps=math.ceil(num_indices/self.neg_indices.shape[0])
+            self.pos_indices=np.repeat(self.pos_indices,num_pos_wraps)[0:num_indices]
+            self.neg_indices=np.repeat(self.neg_indices,num_neg_wraps)[0:num_indices]
+            
+    def __len__(self):
+        return math.floor(self.data.shape[0]/self.batch_size)
+
+    def __getitem__(self,idx):
+        with self.lock:
+            if self.shuffled_ref_negatives==True:
+                self.get_shuffled_ref_negatives_batch(idx)
+            elif self.upsample==True:
+                self.get_upsampled_positives_batch(idx)
+            else:
+                self.get_basic_batch(idx) 
+        
+    def get_shuffled_ref_negatives_batch(self,idx): 
+        #get seq positions
+        inds=self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
+        bed_entries=self.data.index[inds]
+        #get sequences
+        seqs=[self.ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
+        if self.add_revcomp==True:
             #add in the reverse-complemented sequences for training.
             seqs_rc=[revcomp(s) for s in seqs]
             seqs=seqs+seqs_rc
@@ -76,118 +107,69 @@ def shuffled_ref_generator(data_path,ref_fasta,batch_size=128,add_revcomp=True,t
         #generate the corresponding negative set by dinucleotide-shuffling the sequences
         seqs_shuffled=[dinuc_shuffle(s) for s in seqs]
         seqs=seqs+seqs_shuffled
-
         #one-hot-encode the fasta sequences 
         seqs=np.array([[ltrdict.get(x,[0,0,0,0]) for x in seq] for seq in seqs])
-        
         x_batch=np.expand_dims(seqs,1)
-        y_batch=np.asarray(data[start_index:end_index])
-        if add_revcomp==True:
+        y_batch=np.asarray(self.data.iloc[inds])
+        if self.add_revcomp==True:
             y_batch=np.concatenate((y_batch,y_batch),axis=0)
         y_shape=y_batch.shape 
         y_batch=np.concatenate((y_batch,np.zeros(y_shape)))
-        num_generated+=batch_size
-        start_index=end_index
-        yield tuple([x_batch,y_batch])
+        return (x_batch,y_batch)
 
-
-def data_generator(data_path,ref_fasta,batch_size=128,add_revcomp=True,tasks=None,upsample=True,upsample_ratio=0.1):
-    if upsample==False:
-        return data_generator_bed(data_path, ref_fasta,batch_size,add_revcomp,tasks)
-    else:
-        return data_generator_bed_upsample(data_path, ref_fasta, batch_size, add_revcomp, tasks, upsample_ratio)
-
-def data_generator_bed_upsample(data_path,ref_fasta,batch_size=128,add_revcomp=True,tasks=None,upsample_ratio=0.1):
-    #open the reference file
-    ref=pysam.FastaFile(ref_fasta)
-    #load the train data as a pandas dataframe, skip the header
-    data=pd.read_csv(bed_source,header=0,sep='\t',index_col=[0,1,2])
-    if tasks!=None:
-        data=data[tasks]
-    ones = data.loc[(data > 0).any(axis=1)]
-    zeros = data.loc[(data < 1).all(axis=1)]
-    #decide if reverse complement should be used
-    if add_revcomp==True:
-        batch_size=batch_size/2
-    else:
-        batch_size=batch_size
-    pos_batch_size = int(batch_size * upsample_ratio)
-    neg_batch_size = batch_size - pos_batch_size
-    #iterate through batches and one-hot-encode on the fly
-    pos_start_index = 0
-    pos_num_generated = 0
-    pos_total_entries = ones.shape[0] - pos_batch_size
-    neg_start_index = 0
-    neg_num_generated = 0
-    neg_total_entries = zeros.shape[0] - neg_batch_size
-    while True:
-        if (pos_num_generated >= pos_total_entries):
-            pos_start_index=0
-            ones = pd.concat([ones[pos_num_generated:], ones[:pos_num_generated]])
-            pos_num_generated = 0
-        if (neg_num_generated >= neg_total_entries):
-            neg_start_index = 0
-            zeros = pd.concat([zeros[neg_num_generated:], zeros[:neg_num_generated]])
-            neg_num_generated = 0
-        pos_end_index = pos_start_index + int(pos_batch_size)
-        neg_end_index = neg_start_index + int(neg_batch_size)
+    def upsample_positives_batch(idx):
         #get seq positions
-        pos_bed_entries=[(ones.index[i]) for i in range(pos_start_index,pos_end_index)]
-        neg_bed_entries=[(zeros.index[i]) for i in range(neg_start_index, neg_end_index)]
-        bed_entries = pos_bed_entries + neg_bed_entries
+        pos_inds=self.pos_indices[idx*self.pos_batch_size:(idx+1)*self.pos_batch_size]
+        pos_bed_entries=self.ones.index[pos_inds]
+        neg_inds=self.neg_indices[idx*self.neg_batch_size:(idx+1)*self.neg_batch_size]
+        neg_bed_entries=self.zeros.index[neg_inds]
+        bed_entries=pos_bed_entries+neg_bed_entries
+
         #get sequences
-        seqs=[ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
-        if add_revcomp==True:
+        seqs=[self.ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
+        if self.add_revcomp==True:
             #add in the reverse-complemented sequences for training.
             seqs_rc=[revcomp(s) for s in seqs]
             seqs=seqs+seqs_rc
+            
+        #one-hot-encode the fasta sequences 
         seqs=np.array([[ltrdict.get(x,[0,0,0,0]) for x in seq] for seq in seqs])
         x_batch=np.expand_dims(seqs,1)
-        y_labels_ones = ones[pos_start_index:pos_end_index]
-        y_labels_zeros = zeros[neg_start_index:neg_end_index]
-        y_batch_ones = np.asarray(y_labels_ones)
-        y_batch_zeros = np.asarray(y_labels_zeros)
-        y_batch = np.concatenate([y_batch_ones, y_batch_zeros])
-        if add_revcomp==True:
+        
+        #extract the positive and negative labels at the current batch of indices
+        y_batch_pos=self.ones.iloc[pos_inds]
+        y_batch_neg=self.zeros.iloc[neg_inds]
+        y_batch=np.concatenate((y_batch_pos,y_batch_neg),axis=0)
+        #add in the labels for the reverse complement sequences, if used 
+        if self.add_revcomp==True:
             y_batch=np.concatenate((y_batch,y_batch),axis=0)
-        pos_num_generated += pos_batch_size
-        neg_num_generated += neg_batch_size
-        pos_start_index = pos_end_index
-        neg_start_index = neg_end_index
-        yield tuple([x_batch,y_batch])
-
-def data_generator_bed(data_path,ref_fasta,batch_size=128,add_revcomp=True,tasks=None):
-    #open the reference file
-    ref=pysam.FastaFile(ref_fasta)
-    data=pd.read_csv(bed_source,header=0,sep='\t',index_col=[0,1,2])
-    if tasks!=None:
-        data=data[tasks]
-    #iterate through batches and one-hot-encode on the fly
-    start_index=0
-    num_generated=0
-    total_entries=data.shape[0]-batch_size
-    #decide if reverse complement should be used
-    if add_revcomp==True:
-        batch_size=batch_size/2
-    else:
-        batch_size=batch_size
-    while True:
-        if (num_generated >=total_entries):
-            start_index=0
-        end_index=start_index+int(batch_size)
+        return (x_batch,y_batch)            
+    
+    def get_basic_batch(idx):
         #get seq positions
-        bed_entries=[(data.index[i]) for i in range(start_index,end_index)]
+        inds=self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
+        bed_entries=self.data.index[inds]
         #get sequences
-        seqs=[ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
-        if add_revcomp==True:
+        seqs=[self.ref.fetch(i[0],i[1],i[2]) for i in bed_entries]
+        if self.add_revcomp==True:
             #add in the reverse-complemented sequences for training.
             seqs_rc=[revcomp(s) for s in seqs]
             seqs=seqs+seqs_rc
+        #one-hot-encode the fasta sequences 
         seqs=np.array([[ltrdict.get(x,[0,0,0,0]) for x in seq] for seq in seqs])
         x_batch=np.expand_dims(seqs,1)
-        y_batch=np.asarray(data[start_index:end_index])
-        if add_revcomp==True:
+        #extract the labels at the current batch of indices 
+        y_batch=np.asarray(self.data.iloc[inds])
+        #add in the labels for the reverse complement sequences, if used 
+        if self.add_revcomp==True:
             y_batch=np.concatenate((y_batch,y_batch),axis=0)
-        num_generated+=batch_size
-        start_index=end_index
-        yield tuple([x_batch,y_batch])
+        return (x_batch,y_batch)    
+    
+    def on_epoch_end(self):
+        #if upsampling is being used, shuffle the positive and negative indices 
+        if self.upsample==True:
+            np.random.shuffle(self.pos_indices)
+            np.random.shuffle(self.neg_indices)
+        else:
+            np.random.shuffle(self.indices)
+            
