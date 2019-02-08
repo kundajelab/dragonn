91a13d0da396596f6ab952eb52cba9ec8ba43187
anna shcherbina <annashcherbina@gmail.com>
2019-02-08 11:21:13 -0800

    set numpy required version to 1.15. Needed for now to avoid issues in 1.16 (latest release) with saving of pandas to hdf5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'version': '0.2',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy>=1.9', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'simdna==0.3', 'matplotlib',
+    'install_requires': ['numpy==1.15', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'simdna==0.3', 'matplotlib',
                          'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader'],
     'extras_requires':{'tensorflow with gpu':['tensorflow-gpu>=1.7']},
     'dependency_links': ["https://github.com/kundajelab/simdna/tarball/0.3#egg=simdna-0.3",
