bb8eb2e6e6ae3edb706629bda65a8413bd2c15d9
anna shcherbina <annashcherbina@gmail.com>
2019-02-17 18:10:09 -0800

    simdna dependency update in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup,find_packages
 config = {
     'include_package_data': True,
     'description': 'Deep RegulAtory GenOmic Neural Networks (DragoNN)',
-    'version': '0.2.1',
+    'version': '0.2.2',
     'packages': ['dragonn'],
     'setup_requires': [],
     'install_requires': ['numpy>=1.15', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'matplotlib',
-                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader>=0.117'],
+                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader>=0.117','simdna>=0.3'],
     'extras_requires':{'tensorflow with gpu':['tensorflow-gpu>=1.7']},
-    'dependency_links': ["https://github.com/kundajelab/simdna/archive/0.3.zip"],
+    'dependency_links': [],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__main__:main']},
     'name': 'dragonn'
