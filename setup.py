e7cadbeaab0d826959360fe907796c85e8609f19
anna shcherbina <annashcherbina@gmail.com>
2019-02-17 17:22:13 -0800

    changed manner in which simdna dependency is installed

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup,find_packages
 config = {
     'include_package_data': True,
     'description': 'Deep RegulAtory GenOmic Neural Networks (DragoNN)',
-    'version': '0.2',
+    'version': '0.2.1',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy==1.15', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'simdna==0.3', 'matplotlib',
+    'install_requires': ['numpy>=1.15', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'matplotlib',
                          'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader'],
     'extras_requires':{'tensorflow with gpu':['tensorflow-gpu>=1.7']},
     'dependency_links': ["https://github.com/kundajelab/simdna/archive/0.3.zip",
