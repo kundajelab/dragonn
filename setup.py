75fde2dab6e5d95192e6b13883220ccf12ffc2ea
anna shcherbina <annashcherbina@gmail.com>
2019-02-28 00:11:53 -0800

    version increment

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup,find_packages
 config = {
     'include_package_data': True,
     'description': 'Deep RegulAtory GenOmic Neural Networks (DragoNN)',
-    'version': '0.2.5',
+    'version': '0.2.6',
     'packages': ['dragonn'],
     'setup_requires': [],
     'install_requires': ['numpy>=1.15', 'keras>=2.2.0','tensorflow>=1.6','deeplift>=0.6.8.1', 'shapely', 'matplotlib',
-                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader>=0.120','simdna_dragonn'],
+                         'scikit-learn', 'pydot_ng==1.0.0', 'h5py','concise','seqdataloader>=0.121','simdna_dragonn'],
     'extras_requires':{'tensorflow with gpu':['tensorflow-gpu>=1.7']},
     'dependency_links': [],
     'scripts': [],
