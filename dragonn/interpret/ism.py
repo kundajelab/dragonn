172e1c71ff73c351b195c47ea6a89a24cc83e259
anna shcherbina <annashcherbina@gmail.com>
2019-07-10 23:03:54 -0700

    ism update

--- dragonn/interpret/ism.py
+++ dragonn/interpret/ism.py
@@ -7,7 +7,8 @@ def get_preact_function(model,target_layer_idx):
         preact_model=Model(inputs=model.input,
                            outputs=model.layers[target_layer_idx].output)
         return preact_model.predict
-def in_silico_mutagenesis(model, X, task_index,target_layer_idx=-2,start_pos=None,end_pos=None):
+
+def in_silico_mutagenesis(model, X, task_index,target_layer_idx=-2):
     """
     Parameters                               
     ----------                                
@@ -25,14 +26,9 @@ def in_silico_mutagenesis(model, X, task_index,target_layer_idx=-2,start_pos=Non
     
     #Initialize mutants array to the same shape                                     
     output_dim=wild_type_logits.shape+X.shape[2:4]
-    wt_expanded=np.zeros(output_dim)
-    mutants_expanded=np.zeros(output_dim)
+    wt_expanded=np.empty(output_dim)
+    mutants_expanded=np.empty(output_dim)
     empty_onehot=np.zeros(output_dim[3])
-    if start_pos is None:
-        start_pos=0
-    if end_pos is None:
-        end_pos=output_dim[2] 
-
     #3. Iterate through all tasks, positions
     for sample_index in range(output_dim[0]):
         print("ISM: task:"+str(task_index)+" sample:"+str(sample_index))
@@ -40,22 +36,17 @@ def in_silico_mutagenesis(model, X, task_index,target_layer_idx=-2,start_pos=Non
         wt_logit_for_task_sample=wild_type_logits[sample_index]
         wt_expanded[sample_index]=np.tile(wt_logit_for_task_sample,(output_dim[2],output_dim[3]))
         #mutagenize each position
-        temp_batch = []
-        tempbatch_baseposandletter = []
-        for base_pos in range(start_pos,end_pos):
+
+        for base_pos in range(output_dim[2]):
             #for each position, iterate through the 4 bases
             for base_letter in range(output_dim[3]):
                 cur_base=np.array(empty_onehot)
                 cur_base[base_letter]=1
-                Xtmp=np.array(X[sample_index])
-                Xtmp[0][base_pos]=cur_base
-                temp_batch.append(Xtmp)
-                tempbatch_baseposandletter.append((base_pos, base_letter))
-        #get the logits of the batch
-        batch_logits = preact_function([temp_batch]) 
-        for logit,(base_pos, base_letter) in zip(batch_logits, tempbatch_baseposandletter):
-            mutants_expanded[sample_index][0][base_pos][base_letter]=logit
-                
+                Xtmp=np.array(np.expand_dims(X[sample_index],axis=0))
+                Xtmp[0][0][base_pos]=cur_base
+                #get the logit of Xtmp
+                Xtmp_logit=np.squeeze(preact_function(Xtmp),axis=0)
+                mutants_expanded[sample_index][0][base_pos][base_letter]=Xtmp_logit[task_index]
     #subtract wt_expanded from mutants_expanded
     ism_vals=mutants_expanded-wt_expanded
     #For each position subtract the mean ISM score for that position from each of the 4 values
