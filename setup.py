71ae2ee0ec93f9807c392d8ffe151fe7c126a2e7
anna shcherbina <annashcherbina@gmail.com>
2020-09-30 20:57:42 -0700

    tf version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup,find_packages
 config = {
     'include_package_data': True,
     'description': 'Deep RegulAtory GenOmic Neural Networks (DragoNN)',
-    'version': '0.4',
+    'version': '0.4.1',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy==1.16', 'keras>=2.2','tensorflow-gpu>=2.2','deeplift>=0.6.9.0', 'shapely', 'matplotlib>=3.1.1', 'plotnine==0.4.0','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
+    'install_requires': ['numpy==1.16', 'keras==2.3.0','tensorflow-gpu>=1.15','deeplift>=0.6.9.0', 'shapely', 'matplotlib>=3.1.1', 'plotnine==0.4.0','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
     'dependency_links': [],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__init__:main']},
