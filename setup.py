f57755e3b91ccb71dec82898b5ac983d5f8b34a2
anna shcherbina <annashcherbina@gmail.com>
2022-01-25 21:09:28 -0800

    unpin numpy/matplotlib; pin tensorflow-gpu

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'version': '0.4.1',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['numpy==1.16', 'keras==2.3.0','tensorflow-gpu>=1.15','deeplift>=0.6.9.0', 'shapely', 'matplotlib>=3.1.1', 'plotnine==0.4.0','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
+    'install_requires': ['numpy>=1.16', 'keras==2.3.0','tensorflow-gpu==1.15','deeplift>=0.6.9.0', 'shapely', 'matplotlib>=3.1.1', 'plotnine==0.4.0','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
     'dependency_links': [],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__init__:main']},
