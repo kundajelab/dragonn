2f7f153e06b490c9a78da29b4572ec52adef0c79
Surag <10693069+suragnair@users.noreply.github.com>
2022-06-29 22:11:43 +0000

    tf2 update for losses (tf.is_nan -> tf.math.is_nan)

--- dragonn/custom_losses.py
+++ dragonn/custom_losses.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 #ambiguous values are indicated with =np.nan
 def ambig_mean_squared_error(y_true, y_pred):
-    nonAmbig=tf.math.logical_not(tf.is_nan(y_true))
+    nonAmbig=tf.math.logical_not(tf.math.is_nan(y_true))
     return K.mean(K.square(tf.boolean_mask(y_pred,nonAmbig) - tf.boolean_mask(y_true,nonAmbig)), axis=-1)
 
 def ambig_binary_crossentropy(y_true,y_pred):
-    nonAmbig=tf.math.logical_not(tf.is_nan(y_true))
+    nonAmbig=tf.math.logical_not(tf.math.is_nan(y_true))
     return K.mean(K.binary_crossentropy(tf.boolean_mask(y_true,nonAmbig), tf.boolean_mask(y_pred,nonAmbig)), axis=-1);
