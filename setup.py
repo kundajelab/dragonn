53505ba0f97239a9da9b3a06a7737661c6b40149
Surag <10693069+suragnair@users.noreply.github.com>
2022-07-01 20:42:11 +0000

    unpinning matplotlib

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'version': '0.4.1',
     'packages': ['dragonn'],
     'setup_requires': [],
-    'install_requires': ['deeplift>=0.6.9.0', 'shapely', 'matplotlib>=3.1.1', 'plotnine','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
+    'install_requires': ['deeplift>=0.6.9.0', 'shapely', 'matplotlib', 'plotnine','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
     'dependency_links': [],
     'scripts': [],
     'entry_points': {'console_scripts': ['dragonn = dragonn.__init__:main']},
