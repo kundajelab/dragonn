a0e0006f8c9e398f65e2480c59fb25f79cd63336
Surag <10693069+suragnair@users.noreply.github.com>
2023-07-17 23:14:05 +0000

    added logomaker to setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'version': '0.4.1',
     'packages': find_packages(),
     'setup_requires': [],
-    'install_requires': ['deeplift>=0.6.9.0', 'shapely', 'matplotlib', 'plotnine','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention'],
+    'install_requires': ['deeplift>=0.6.9.0', 'shapely', 'matplotlib', 'plotnine','scikit-learn>=0.20.0', 'pydot_ng==1.0.0', 'h5py','seqdataloader>=0.124','simdna_dragonn','abstention', 'logomaker'],
     'dependency_links': [],
     'scripts': [],
     'name': 'dragonn'
